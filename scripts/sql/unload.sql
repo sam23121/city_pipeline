b752e4143f6bf42ef5f3ef2f5a1279ef69dcf332
sam23121 <smlalene@gmail.com>
2022-07-19 00:07:47 +0300

    adding scripts for sql

--- scripts/sql/unload.sql
+++ scripts/sql/unload.sql
@@ -1,12 +1,17 @@
-COPY (
-       select invoice_number,
-              stock_code,
-              detail,
-              quantity,
-              invoice_date,
-              unit_price,
-              customer_id,
-              country
-       from retail.user_purchase -- we should have a date filter here to pull only required date's data
-) TO '{{ params.user_purchase }}' WITH (FORMAT CSV, HEADER);
+-- COPY (
+--        select track_id,
+--                 'type',
+--          traveled_d,
+--           avg_speed, 
+--           lat, lon, 
+--           speed, 
+--           lon_acc, 
+--           lat_acc,
+--            'time'
+--        from retail.user_purchase -- we should have a date filter here to pull only required date's data
+-- ) TO '{{ params.user_purchase }}' WITH (FORMAT CSV, HEADER);
 -- user_purchase will be replaced with /temp/user_purchase.csv from the params in extract_user_purchase_data task
+COPY employees(track_id, type,traveled_d, avg_speed, lat, lon, speed, lon_acc, lat_acc, time)
+FROM ‘../data/20181029_d1_0800_0830.csv.csv’
+DELIMITER ‘,’
+CSV HEADER;
